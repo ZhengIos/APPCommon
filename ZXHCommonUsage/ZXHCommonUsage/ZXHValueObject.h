9fc3f5028b8105f438bf13a7e7a3e46030a8a41c
policp <zust104_426hai@163.com>
2016-06-28 17:37:27 +0800

    增加了富文本的处理
    1.增加了富文本的处理

--- ZXHCommonUsage/ZXHCommonUsage/ZXHValueObject.h
+++ ZXHCommonUsage/ZXHCommonUsage/ZXHValueObject.h
@@ -37,7 +37,26 @@ long long ZXHHardDriveFreeSize();
  *  @return <#return value description#>
  */
 NSDate * getYearsDate(int years);
+/**
+ *  将字典转换为字符串
+ *
+ *  @param dict <#dict description#>
+ *
+ *  @return <#return value description#>
+ */
 NSString *stringFromDictionary(NSDictionary *dict);
+/**
+ *  返回两种样式的字符串
+ *
+ *  @param string1     <#string1 description#>
+ *  @param color1      <#color1 description#>
+ *  @param string2     <#string2 description#>
+ *  @param color2      <#color2 description#>
+ *  @param spaceLenght <#spaceLenght description#>
+ *
+ *  @return <#return value description#>
+ */
+NSAttributedString *multiStylesStirng(NSString *string1,UIColor *color1,NSString *string2,UIColor *color2,NSInteger spaceLenght);
 //@interface ZXHValueObject : NSObject
 //+(instancetype)sharedValueObject;
 //@end
--- ZXHCommonUsage/ZXHCommonUsage/ZXHValueObject.m
+++ ZXHCommonUsage/ZXHCommonUsage/ZXHValueObject.m
@@ -40,7 +40,20 @@ NSString *stringFromDictionary(NSDictionary *dict)
     NSString *str=[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
     return str;
 }
-
+NSAttributedString *multiStylesStirng(NSString *string1,UIColor *color1,NSString *string2,UIColor *color2,NSInteger spaceLenght)
+{
+    NSInteger length1=string1.length;
+    NSInteger length2=string2.length;
+    NSMutableString *string=[[NSMutableString alloc] initWithString:string1];
+    [string stringByAppendingString:string2];
+    
+    NSDictionary *attriDic1=[[NSDictionary alloc] initWithObjectsAndKeys:color1,NSForegroundColorAttributeName, nil];
+    NSDictionary *attriDic2=[[NSDictionary alloc] initWithObjectsAndKeys:color2,NSForegroundColorAttributeName, nil];
+    NSMutableAttributedString *attributedString=[[NSMutableAttributedString alloc] initWithString:string];
+    [attributedString setAttributes:attriDic1 range:NSMakeRange(0, length1)];
+    [attributedString setAttributes:attriDic2 range:NSMakeRange(length1+spaceLenght, length2)];
+    return attributedString;
+}
 //@implementation ZXHValueObject
 //+(instancetype)sharedValueObject
 //{
