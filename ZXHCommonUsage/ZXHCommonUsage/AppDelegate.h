8f92487609b053d972bfc7036135411f97f05318
policp <zust104_426hai@163.com>
2016-06-28 11:47:02 +0800

    创建项目
    创建了项目

---
+++ ZXHCommonUsage/ZXHCommonUsage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZXHCommonUsage
+//
+//  Created by chenpeng on 16/6/28.
+//  Copyright © 2016年 zhengxh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
